188e125422f849efc0e4e4f8b55b36ad3cecdd86
Yan Lima Barbosa <yanbr763@gmail.com>
2023-08-27 17:59:02 -0300

    Highlight the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,9 +5,10 @@ import GameListSkeleton from "./GameListSkeleton"
 
 interface Props {
   onSelectGenre: (genre: Genre) => void
+  selectedGenre: Genre | null
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres()
   const skeletons = Array.from({ length: 20 })
 
@@ -26,6 +27,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
               src={getCroppedImageUrl(genre.image_background)}
             />
             <Button
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
               fontSize={"lg"}
               variant="link"
